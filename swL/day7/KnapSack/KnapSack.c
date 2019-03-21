dd2a528be4ff5124210469a41cc7bf7f86e40771
jihwanK <jhwan1228@gamil.com>
2019-03-21 16:41:24 +0900

    mission 7

--- swL/day7/KnapSack/KnapSack.c
+++ swL/day7/KnapSack/KnapSack.c
@@ -13,6 +13,14 @@ void initData(void) {
 	 * TO DO
 	 */
 
+	for (int i = 0; i <= MAX; i++) {
+		V[i] = 0;
+		W[i] = 0;
+
+		for (int j = 0; j <= MAX; j++) {
+			dp[i][j] = 0;
+		}
+	}
 }
 
 void createData(void) {
@@ -21,12 +29,17 @@ void createData(void) {
 	 * TO DO
 	 */
 
+	scanf("%d %d", &C, &J);
+
+	for (int i = 1; i <= C; i++) {
+		scanf("%d %d", &W[i], &V[i]);
+	}
 }
 
 void printData(void) {
 	int i;
 
-	for (i=1 ; i<=J ; i++) {
+	for (i=1 ; i<=C ; i++) {
 		printf("%d %d\n", W[i], V[i]);
 	}
 }
@@ -37,5 +50,15 @@ int calc(void) {
 	 * TO DO
 	 */
 
-	return 0;
+	for (int i = 1; i <= C; i++) {
+		for (int j = 1; j <= J; j++) {
+			if (W[i] > j) {
+				dp[i][j] = dp[i - 1][j];
+			} else {
+				dp[i][j] = dp[i - 1][j] > (dp[i - 1][j - W[i]] + V[i]) ? dp[i - 1][j] : (dp[i - 1][j - W[i]] + V[i]);
+			}
+		}
+	}
+
+	return dp[C][J];
 }
