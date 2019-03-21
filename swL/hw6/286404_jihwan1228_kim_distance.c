d1202d522e337dcf6ea82f7a35ef547bfc5439d4
jihwanK <jhwan1228@gmail.com>
2019-03-22 00:20:02 +0900

    solved the hw

--- swL/hw6/286404_jihwan1228_kim_distance.c
+++ swL/hw6/286404_jihwan1228_kim_distance.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
+  int testcase = 0; 
 
+  freopen("data.txt", "r", stdin);
+
+  scanf("%d", &testcase);
+
+  for (int i = 0; i < testcase; i++) {
+    int node_a, node_b;
+    int num_a = 0, num_b = 0;
+
+    scanf("%d %d", &node_a, &node_b);
+
+    while (node_a != node_b) {
+      if (node_a > node_b) {
+        node_a = node_a / 2;
+        num_a++;
+      } else {
+        node_b = node_b / 2;
+        num_b++;
+      }
+    }
+
+    printf("%d \n", num_a + num_b);
+
+  }
   return 0;
 }
