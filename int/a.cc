a59e93e4ba2dee954f72009e75e0daad2356c0c5
jihwanK <jhwan1228@gmail.com>
2021-03-12 12:47:32 +0000

    python interview practice day1

--- int/a.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-#define NAME_SIZE 50
-
-class Person {
-    int id;
-}
--- int/word.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include "word.h"
-
-void makeWord(Word* wp, const char* str) {
-    wp->len = strlen(str);
-    strcpy(wp->word, str);
-}
