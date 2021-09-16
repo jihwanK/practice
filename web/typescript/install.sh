1974c7c4162741a3189970f20b6018017202a762
jihwanK <jhwan1228@gmail.com>
2021-09-16 14:07:11 +0100

    web environment

---
+++ web/typescript/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install nodejs
+conda install -c conda-forge nodejs
+
+# install typescript with npm
+npm install -g typescript
+
+# install node-typescript
+npm install -g ts-node
