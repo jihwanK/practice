9a110e40584c13f8c36cb453d21f47ef785ba80a
Ji Hwan Kim <jhwan1228@gmail.com>
2025-04-13 04:09:55 +0900

    Title: XOR 결과 최대 만들기, Time: 1050ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 250413/XOR 결과 최대 만들기/max-of-xor.py
+++ 250413/XOR 결과 최대 만들기/max-of-xor.py
@@ -1,3 +1,29 @@
+# n, m = map(int, input().split())
+# A = list(map(int, input().split()))
+
+# # Please write your code here.
+# max_value = -1
+# answer = []
+
+# def choose(cnt):
+#     global max_value
+    
+#     if cnt == m:
+#         tmp = answer[0]
+#         for ele in answer[1:]:
+#             tmp ^= ele
+#         max_value = max(max_value, tmp)
+#         return
+    
+#     for a in A:
+#         if not answer or max(answer) > a:
+#             answer.append(a)
+#             choose(cnt+1)
+#             answer.pop()
+
+# choose(0)
+# print(max_value)
+
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
@@ -5,7 +31,7 @@ A = list(map(int, input().split()))
 max_value = -1
 answer = []
 
-def choose(cnt):
+def choose(cnt, last_item):
     global max_value
     
     if cnt == m:
@@ -15,11 +41,10 @@ def choose(cnt):
         max_value = max(max_value, tmp)
         return
     
-    for a in A:
-        if not answer or max(answer) > a:
-            answer.append(a)
-            choose(cnt+1)
-            answer.pop()
+    for i in range(last_item, len(A)):
+        answer.append(A[i])
+        choose(cnt+1, last_item)
+        answer.pop()
 
-choose(0)
+choose(0, 0)
 print(max_value)
