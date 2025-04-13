06a2701708b228df9746f9f6c0f150a39f815ed1
Ji Hwan Kim <jhwan1228@gmail.com>
2025-04-13 10:39:56 +0900

    Title: 사각형 채우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250413/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+# Please write your code here.
+
+MOD = 10007
+
+# 변수 선언 및 입력:
+dp = [0] * (n + 1)
+   
+# 초기 조건 설정
+dp[0] = 1
+dp[1] = 1
+
+# 점화식에 따라 dp값 채우기
+# dp[i] = dp[i - 1] + dp[i - 2]
+for i in range(2, n + 1):
+    # dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[n]%MOD)
