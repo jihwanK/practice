33f5aafd400496f28ac93317332776ccd67668c4
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-02 02:40:38 +0900

    Title: 최대 감소 부분 수열, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250502/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,15 @@
+n = int(input())
+m = list(map(int, input().split()))
+
+# Please write your code here.
+dp = [0]*n
+
+dp[0] = 1
+for i in range(n):
+    for j in range(i+1, n):
+        if m[i] <= m[j]:
+            dp[j] = dp[i]
+        else:
+            dp[j] = dp[i] + 1
+
+print(dp[-1])
