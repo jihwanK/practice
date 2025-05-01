dec0728251e1a4e8de28e5b7661a8dbacfadab6e
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-02 02:51:18 +0900

    Title: 최대 감소 부분 수열, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250502/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 250502/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -4,13 +4,11 @@ m = list(map(int, input().split()))
 # Please write your code here.
 dp = [0]*n
 
-dp[0] = 1
 for i in range(n):
-    for j in range(i+1, n):
-        if m[i] <= m[j]:
-            dp[j] = dp[i]
-            break
-        else:
-            dp[j] = dp[i] + 1
+    dp[i] = 1
 
-print(dp[-1])
+    for j in range(i):
+        if m[j] > m[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
