7c40b461b3c4ddb80c1abea0ac4deb4396935390
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-02 18:36:55 +0900

    Title: 부분 수열의 합, Time: 72ms, Memory: 19MB, Status: Passed - Codetree

--- 250502/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 250502/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -6,7 +6,7 @@ dp = [[False]*(m+1) for _ in range(n+1)]
 
 dp[0][0] = True
 for i in range(1, n+1):
-    for j in range(1, m+1):
+    for j in range(m+1):
         if j-A[i] >= 0 and dp[i-1][j-A[i]] == True:
             dp[i][j] = True
         
