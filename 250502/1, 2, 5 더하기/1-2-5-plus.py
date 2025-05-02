7e98469f8a98ebf13a6c5e182d370bfdafedd44b
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-02 17:04:46 +0900

    Title: 1, 2, 5 더하기, Time: 58ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250502/1, 2, 5 더하기/1-2-5-plus.py
+++ 250502/1, 2, 5 더하기/1-2-5-plus.py
@@ -1,4 +1,15 @@
 n = int(input())
 
 # Please write your code here.
-dp = [0]*n
+dp = [0]*(n+1)
+numbers = [1,2,5]
+
+for i in range(3):
+    dp[numbers[i]] = 1
+
+for i in range(n+1):
+    for j in range(3):
+            if (i-numbers[j]) >= 0:
+                dp[i] = dp[i] + dp[i-numbers[j]]
+
+print(dp[n])
