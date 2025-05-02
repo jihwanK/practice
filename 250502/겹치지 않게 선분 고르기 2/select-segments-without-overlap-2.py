d3d573541ab6b64a0744089b0cfd98b34372a8ea
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-02 15:10:58 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250502/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 250502/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -1,9 +1,20 @@
 n = int(input())
-x1, x2 = [], []
+# x1, x2 = [], []
+lines = []
 for _ in range(n):
     a, b = map(int, input().split())
-    x1.append(a)
-    x2.append(b)
+    lines.append((a, b))
+    # x1.append(a)
+    # x2.append(b)
 
 # Please write your code here.
+def is_overlap(l1, l2):
+    return l1[1] <= l2[0]
+
+lines.sort(key=lambda x: (x[0], x[1]-x[0]))
+
 dp = [0]*n
+
+dp[0] = 1
+for idx, (start, end) in enumerate(lines, start=1):
+    print(idx, start, end)
