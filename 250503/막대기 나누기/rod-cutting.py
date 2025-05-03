bda6bf342ac7ae2e42a9a04cd75035b5695987ec
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-03 17:19:09 +0900

    Title: 막대기 나누기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250503/막대기 나누기/rod-cutting.py
@@ -0,0 +1,12 @@
+n = int(input())
+profit = [-1]+list(map(int, input().split()))
+
+# Please write your code here.
+dp = [0] * (n + 1)
+
+dp[0] = 0
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + profit[j])
+        print((i,j),dp)
