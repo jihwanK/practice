80cc554adefb1080f10b5de8aabf1a1b31ca6eeb
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-04 17:01:00 +0900

    Title: 배낭 채우기 2, Time: 60ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250504/배낭 채우기 2/knapsack-2.py
+++ 250504/배낭 채우기 2/knapsack-2.py
@@ -8,9 +8,8 @@ dp = [[0]*(M+1) for _ in range(N)]
 for i in range(N):
     for j in range(1, M+1):
         if j - w[i] >= 0:
-            # dp[i][j] = max(dp[i][j-w[i]]+v[i], dp[i-1][j-w[i]]+v[i], dp[i-1][j])
             dp[i][j] = max(dp[i][j-w[i]]+v[i], dp[i-1][j])
-        else:
-            dp[i][j] = dp[i-1][j]
+        # else:
+        #     dp[i][j] = dp[i-1][j]
 
 print(max(dp[-1]))
