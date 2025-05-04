90623bf7b7f2f383b23de64c13aaaee8185eae11
Ji Hwan Kim <jhwan1228@gmail.com>
2025-05-04 17:01:21 +0900

    Title: 배낭 채우기 2, Time: 66ms, Memory: 18MB, Status: Passed - Codetree

--- 250504/배낭 채우기 2/knapsack-2.py
+++ 250504/배낭 채우기 2/knapsack-2.py
@@ -9,7 +9,7 @@ for i in range(N):
     for j in range(1, M+1):
         if j - w[i] >= 0:
             dp[i][j] = max(dp[i][j-w[i]]+v[i], dp[i-1][j])
-        # else:
-        #     dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = dp[i-1][j]
 
 print(max(dp[-1]))
