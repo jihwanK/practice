95d1480496390a10a0809a92e0789e1a01dba93c
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-01 15:47:57 +0900

    Title: DateTime to DateTime, Time: 39ms, Memory: 16MB, Status: Passed - Codetree

--- 250601/DateTime to DateTime/datetime-to-datetime.py
+++ 250601/DateTime to DateTime/datetime-to-datetime.py
@@ -6,4 +6,7 @@ start_time, end_time = 0, 0
 end_time += a*24*60 + b*60 + c
 start_time += 11*24*60 + 11*60 + 11
 
-print(end_time-start_time)
+if end_time-start_time < 0:
+    print(-1)
+else:
+    print(end_time-start_time)
