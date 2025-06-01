d1ba8f52e1c4529c4ee7b1b29e3c6d413a94427e
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-01 16:41:09 +0900

    Title: 최대로 겹치는 구간, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250601/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -4,7 +4,7 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 axis = [0]*(201)
 for segment_a, segment_b in segments:
-    for i in range(segment_a+100, segment_b+101):
+    for i in range(segment_a+100, segment_b+100):
         axis[i] += 1
 
 max_overlapped = 0
@@ -12,6 +12,8 @@ for i in range(200):
     if axis[i] == axis[i+1]:
         if max_overlapped < axis[i]:
             max_overlapped = axis[i]
+            # print(i, max_overlapped)
 
 # print(axis)
 print(max_overlapped)
+# print(max(axis))
