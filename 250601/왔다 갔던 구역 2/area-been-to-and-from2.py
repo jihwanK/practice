13f5b3dbec1e84df7ff2dd071d481040bf02acad
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-01 17:30:24 +0900

    Title: 왔다 갔던 구역 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250601/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -15,7 +15,7 @@ for i in range(n):
     if dir[i] == "R":
         for delta in range(x[i]+1):
             trace[cur+delta] += 1
-        cur += x[i] 
+        cur += x[i]
     elif dir[i] == "L":
         for delta in range(x[i]+1):
             trace[cur-delta] += 1
@@ -27,5 +27,6 @@ for i in range(2000):
         answer += 1
 
 # print(len(list(filter(lambda x: x>=2, trace))))
+# print(trace[980:1020])
 print(answer)
 
