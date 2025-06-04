f782d113812b2d50d51e0487a552e9f2e01b8869
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-04 16:47:46 +0900

    Title: 연속되는 수 3, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250604/연속되는 수 3/continuous-number3.py
+++ 250604/연속되는 수 3/continuous-number3.py
@@ -2,5 +2,19 @@ N = int(input())
 arr = [int(input()) for _ in range(N)]
 
 # Please write your code here.
+def sign(num):
+    if num > 0:
+        return 1
+    else:
+        return -1
+
+cnt = 1
+ans = 1
 for i in range(1, N):
     if sign(arr[i-1]) != sign(arr[i]):
+        ans = max(ans, cnt)
+        cnt = 1
+    else:
+        cnt += 1
+
+print(ans)
