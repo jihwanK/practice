143bd772b0f7df0f938666da699872b70bf3a041
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-11 16:15:00 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250611/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250611/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -5,18 +5,15 @@ handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 handshakes.sort(key=lambda x: (x[0], x[1], x[2]))
 
 developers = [0]*(N+1)
+transmissibility = [K]*(N+1)
 developers[P] = 1 # 최초 감염자
 
-count = 0
 for t, x, y in handshakes:
-    if count == K:
-        # print(t, x, y, P)
-        break
-
-    if developers[x] or developers[y]:
-        developers[x] = 1
+    if developers[x] and transmissibility[x] > 0:
         developers[y] = 1
-        count += 1
-        # print(t, x, y)
+        transmissibility[x] -= 1
+    elif developers[y] and transmissibility[y] >0:
+        developers[x] = 1
+        transmissibility[y] -= 1
 
 print(''.join(map(str, developers[1:])))
