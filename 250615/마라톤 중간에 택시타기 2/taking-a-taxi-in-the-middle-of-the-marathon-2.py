5e8aaa0f3c3c5771574e86a8785ece486a5995e5
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-15 02:32:12 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250615/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,16 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+min_dist = 1e10
+prev = points[0]
+for skip_point in points[1:n]:
+    dist = 0
+    for point in points:
+        if point != skip_point:
+            dist += (abs(point[0]-prev[0]) + abs(point[1]-prev[1]))
+            prev = point
+    min_dist = min(min_dist, dist)
+print(min_dist)
