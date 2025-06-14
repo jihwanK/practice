e42945a731db06591b51e3cb859d6a4ff87b163f
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-15 02:03:06 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250615/이상한 진수 2/awkward-digits-2.py
+++ 250615/이상한 진수 2/awkward-digits-2.py
@@ -1,10 +1,15 @@
 a = input()
 
 # Please write your code here.
-binary = list(a)
-for i in range(len(binary)):
-    if binary[i] == '0':
-        binary[i] = '1'
-        break
-print(int(''.join(binary), 2))
+
+if a == 1:
+    print(0)
+else:
+    binary = list(a)
+    for i in range(len(binary)):
+        if binary[i] == '0':
+            binary[i] = '1'
+            break
+
+    print(int(''.join(binary), 2))
 
