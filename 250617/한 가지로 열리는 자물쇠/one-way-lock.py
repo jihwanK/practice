5ac616338689bc173261699664e2499a89d62c70
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-17 18:43:32 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250617/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 250617/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -2,10 +2,15 @@ N = int(input())
 a, b, c = map(int, input().split())
 
 # Please write your code here.
-answer = 0
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        for k in range(1, N+1):
-            if (abs(i-a) <= 2) or (abs(j-b) <= 2) or (abs(k-c) <= 2):
-                answer += 1
-print(answer)
+# answer = 0
+# for i in range(1, N+1):
+#     for j in range(1, N+1):
+#         for k in range(1, N+1):
+#             if (abs(i-a) <= 2) or (abs(j-b) <= 2) or (abs(k-c) <= 2):
+#                 answer += 1
+# print(answer)
+
+i = len(range(max(1, a-2), min(N, a+2)+1))
+j = len(range(max(1, b-2), min(N, b+2)+1))
+k = len(range(max(1, c-2), min(N, c+2)+1))
+print((i+j+k)*N*N - i*j*N - j*k*N - k*i*N + i*j*k)
