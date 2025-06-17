41c5d518dcd591e9cef7fc704ab1c8f95db59433
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-17 19:33:33 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 60ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250617/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 250617/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -3,9 +3,27 @@ a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 
 # Please write your code here.
+def is_adjecent(a, b):
+    if abs(a-b) <= 2:
+        return True
+    if a == N:
+        if abs(a%N - b) <= 2:
+            return True
+        else:
+            return False
+    elif a == N-1:
+        if b == N or b == 1:
+            return True
+        else:
+            return False
+    return False
+    
+
+answer = 0
 for i in range(1, N+1):
     for j in range(1, N+1):
         for k in range(1, N+1):
-            if (abs(i-a1) <= 2 and abs(j-b1) <= 2 and abs(k-c1)) or \
-                (abs(i-a2) <= 2 and abs(j-b2) <= 2 and abs(k-c2)):
+            if (is_adjecent(i, a1) and is_adjecent(j, b1) and is_adjecent(k, c1)) or \
+                (is_adjecent(i, a2) and is_adjecent(j, b2) and is_adjecent(k, c2)):
                 answer += 1
+print(answer)
