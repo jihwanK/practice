326d7c771ec30139e14c4d65a06124f164fb0820
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-17 17:05:18 +0900

    Title: 특정 구간의 원소 평균값, Time: 60ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250617/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+answer = 0
+
+for length in range(1, n):
+    for i in range(n-length+1):
+        if sum(arr[i:i+length])/length in arr[i:i+length]:
+            answer += 1
+print(answer)
