914544343c1695bc2a5f078efee7764742135c41
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-27 17:00:53 +0900

    Title: 겹치지 않는 선분 2, Time: 59ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250627/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 250627/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -3,15 +3,18 @@ lines = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 def is_intersect(line1, line2):
-    return (line1[0] < line2[0] < line1[1] and line1[0] < line2[1] < line1[1]) \
-    or (line2[0] < line1[0] < line2[1] and line2[0] < line1[1] < line2[1])
+    return (line1[0] < line1[1] and line1[0] < line2[0] < line1[1] and line1[0] < line2[1] < line1[1]) \
+        or (line2[0] < line2[1] and line2[0] < line1[0] < line2[1] and line2[0] < line1[1] < line2[1]) \
+        or (line1[0] > line1[1] and line1[1] < line2[0] < line1[0] and line1[1] < line2[1] < line1[0]) \
+        or (line2[0] > line2[1] and line2[1] < line1[0] < line2[0] and line2[1] < line1[1] < line2[0])
 
 answer = 0
 intersect = False
 for i in range(n):
     for j in range(n):
-        if is_intersect(lines[i], lines[j]):
+        if i != j and is_intersect(lines[i], lines[j]):
             intersect = True
+            break
     if not intersect:
         answer += 1
     intersect = False
