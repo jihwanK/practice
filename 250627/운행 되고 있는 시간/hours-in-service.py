313c501891aa5288f9001cfbf31bee7ac1d45918
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-27 16:24:02 +0900

    Title: 운행 되고 있는 시간, Time: 63ms, Memory: 17MB, Status: Passed - Codetree

--- 250627/운행 되고 있는 시간/hours-in-service.py
+++ 250627/운행 되고 있는 시간/hours-in-service.py
@@ -5,12 +5,11 @@ b = [t[1] for t in times]
 
 # Please write your code here.
 answer = 0
-working_hours = [0]*1001
 for i in range(n):
+    working_hours = [0]*1001
     for worker in range(n):
         if worker != i:
             for time in range(a[worker], b[worker]):
                 working_hours[time] = 1
     answer = max(answer, sum(working_hours))
-    working_hours = [0]*1001
 print(answer)
