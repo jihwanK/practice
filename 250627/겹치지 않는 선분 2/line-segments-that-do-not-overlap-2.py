36ae2530088ec425592481aeb45040ed23263aac
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-27 17:28:18 +0900

    Title: 겹치지 않는 선분 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250627/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 250627/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -6,7 +6,9 @@ def is_intersect(line1, line2):
     return (line1[0] < line1[1] and line1[0] < line2[0] < line1[1] and line1[0] < line2[1] < line1[1]) \
         or (line2[0] < line2[1] and line2[0] < line1[0] < line2[1] and line2[0] < line1[1] < line2[1]) \
         or (line1[0] > line1[1] and line1[1] < line2[0] < line1[0] and line1[1] < line2[1] < line1[0]) \
+        or (line1[0] > line1[1] and line2[0] < line1[0] < line2[1] and line2[0] < line1[1] < line2[1]) \
         or (line2[0] > line2[1] and line2[1] < line1[0] < line2[0] and line2[1] < line1[1] < line2[0]) \
+        or (line2[0] > line2[1] and line1[0] < line2[0] < line1[1] and line1[0] < line2[1] < line1[1]) \
         or (line1[0] < line1[1] and line2[0] > line2[1] and line1[1] > line2[1] and line2[0] > line1[1]) \
         or (line1[0] > line1[1] and line2[0] < line2[1] and line2[1] > line1[1] and line1[0] > line2[1])
 
