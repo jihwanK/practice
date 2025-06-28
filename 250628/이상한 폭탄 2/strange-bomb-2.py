8c6f30827895366957c2e250243a28dd5080305f
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-28 12:27:22 +0900

    Title: 이상한 폭탄 2, Time: 69ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250628/이상한 폭탄 2/strange-bomb-2.py
+++ 250628/이상한 폭탄 2/strange-bomb-2.py
@@ -3,11 +3,12 @@ num = [int(input()) for _ in range(N)]
 
 # Please write your code here.
 answer = -1
-for bomb_num in range(1, N+1):
+for bomb_num in range(1001):
     for i in range(N):
         for j in range(N):
             if i == j or num[i] != bomb_num: continue
             if num[i] == num[j] and j-i <= K:
                 answer = max(answer, bomb_num)
+                break
 print(answer)
                 
