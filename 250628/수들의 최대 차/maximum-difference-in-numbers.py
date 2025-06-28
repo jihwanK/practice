02db55acdb580234050fbf2ee93c8e6c5ed41f05
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-28 19:21:51 +0900

    Title: 수들의 최대 차, Time: 558ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250628/수들의 최대 차/maximum-difference-in-numbers.py
+++ 250628/수들의 최대 차/maximum-difference-in-numbers.py
@@ -6,8 +6,8 @@ from itertools import combinations
 
 flag = False
 for n in range(N, 0, -1):
-    comb = list(combinations(arr, n))
-    for chosen in comb:
+    # comb = 
+    for chosen in list(combinations(arr, n)):
         if max(chosen) - min(chosen) <= K:
             print(n)
             flag = True
