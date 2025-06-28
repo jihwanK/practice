31b5afdbf44e7f93cb7904d7365472a466376d5d
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-28 18:43:10 +0900

    Title: 가장 작은 x 찾기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250628/가장 작은 x 찾기/find-smallest-x.py
+++ 250628/가장 작은 x 찾기/find-smallest-x.py
@@ -4,6 +4,7 @@ a, b = zip(*ranges)
 a, b = list(a), list(b)
 
 # Please write your code here.
+flag = False
 for x in range(1, 10001):
     for n in range(1, N+1):
         if x*2**n not in range(a[n-1], b[n-1]+1):
