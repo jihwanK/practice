20f34d1b7f4a9b89a17f258b450d7105bb8ed709
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-28 19:50:16 +0900

    Title: 수들의 최대 차, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250628/수들의 최대 차/maximum-difference-in-numbers.py
+++ 250628/수들의 최대 차/maximum-difference-in-numbers.py
@@ -2,14 +2,36 @@ N, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 
 # Please write your code here.
-from itertools import combinations
+# from itertools import combinations
 
-flag = False
-for n in range(N, 0, -1):
-    # comb = 
-    for chosen in list(combinations(arr, n)):
-        if max(chosen) - min(chosen) <= K:
-            print(n)
-            flag = True
-            break
-    if flag: break
+# flag = False
+# for n in range(N, 0, -1):
+#     for chosen in combinations(arr, n):
+#         if max(chosen) - min(chosen) <= K:
+#             print(n)
+#             flag = True
+#             break
+#     if flag: break
+
+arr.sort()
+# print(arr)
+i, j = 0, N-1
+answer = 0
+while i < j:
+    max_val, min_val = arr[j], arr[i]
+    if max_val - min_val <= K:
+        answer = max(answer, j-i+1)
+        # print(j, i)
+        break
+    i += 1
+
+i, j = 0, N-1
+while i < j:
+    max_val, min_val = arr[j], arr[i]
+    if max_val - min_val <= K:
+        answer = max(answer, j-i+1)
+        # print(j, i)
+        break
+    j -= 1
+
+print(answer)
