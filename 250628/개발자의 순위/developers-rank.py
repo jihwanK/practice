ad96fe3eb0fa7bb96f12f8cbbc2124ef1b8579f5
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-28 12:09:03 +0900

    Title: 개발자의 순위, Time: 67ms, Memory: 17MB, Status: Passed - Codetree

--- 250628/개발자의 순위/developers-rank.py
+++ 250628/개발자의 순위/developers-rank.py
@@ -5,9 +5,9 @@ arr = [list(map(int, input().split())) for _ in range(k)]
 from collections import defaultdict
 
 counter = defaultdict(int)
-for i in range(k):
-    for j in range(n):
-        for k in range(j+1, n):
-            counter[(arr[i][j], arr[i][k])] += 1
+for idx in range(k):
+    for i in range(n):
+        for j in range(i+1, n):
+            counter[(arr[idx][i], arr[idx][j])] += 1
 
 print(len(list(filter(lambda v: v == k, counter.values()))))
