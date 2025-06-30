58af36e96eb9c226939f39b7cd0a378674124bbf
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-30 20:57:11 +0900

    Title: 초기 수열 복원하기, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250630/초기 수열 복원하기/restore-initial-sequence.py
+++ 250630/초기 수열 복원하기/restore-initial-sequence.py
@@ -8,7 +8,7 @@ for first in range(1, n+1):
     answer[0] = first
     for i in range(n-1):
         next_val = adjacent[i] - answer[i]
-        if next_val not in answer:
+        if next_val > 0 and next_val not in answer:
             answer[i+1] = next_val
             is_successful = True
         else:
