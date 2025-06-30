c07a670bc2d3a711d83c69db48734c5f0b9c00f3
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-30 20:01:37 +0900

    Title: A, B, C, D 찾기 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250630/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 250630/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -3,11 +3,25 @@ nums = list(map(int, input().split()))
 # Please write your code here.
 nums.sort()
 a = nums[0]
-for b in range(a, 41):
-    for c in range(b, 41):
-        for d in range(c, 41):
-            if a+b+c+d != nums[-1] \
-                or b+c+d != nums[-2] \
-                or a+c+d != nums[-3] \
-                or a+b+d != nums[-4]: continue
-            print(a,b,c,d)
+b = nums[1]
+candidates = [0]*15
+for c in range(b, 41):
+    for d in range(c, 41):
+        if a+b+c+d != nums[-1]: continue
+        candidates[0] = a
+        candidates[1] = b
+        candidates[2] = c
+        candidates[3] = d
+        candidates[4] = a + b
+        candidates[5] = b + c
+        candidates[6] = c + d
+        candidates[7] = d + a
+        candidates[8] = a + c
+        candidates[9] = b + d
+        candidates[10] = a + b + c
+        candidates[11] = a + b + d
+        candidates[12] = a + c + d
+        candidates[13] = b + c + d
+        candidates[14] = a + b + c + d
+        if nums == sorted(candidates):
+            print(a, b, c, d)
