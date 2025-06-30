cab7b9093e8c42ccf2b89566d469665976d1d062
Ji Hwan Kim <jhwan1228@gmail.com>
2025-06-30 20:52:51 +0900

    Title: 초기 수열 복원하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250630/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,20 @@
+n = int(input())
+adjacent = list(map(int, input().split()))
+
+# Please write your code here.
+is_successful = False
+answer = [0]*n
+for first in range(1, n+1):
+    answer[0] = first
+    for i in range(n-1):
+        next_val = adjacent[i] - answer[i]
+        if next_val not in answer:
+            answer[i+1] = next_val
+            is_successful = True
+        else:
+            is_successful = False
+            break
+    if is_successful:
+        break
+
+print(*answer)
