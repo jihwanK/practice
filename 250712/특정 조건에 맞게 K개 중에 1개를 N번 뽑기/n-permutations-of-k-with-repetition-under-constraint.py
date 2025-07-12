e7ad27234a7d9e86a096c7172db822d14f9f59d0
Ji Hwan Kim <jhwan1228@gmail.com>
2025-07-12 18:56:15 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 284ms, Memory: 26MB, Status: Passed - Codetree

--- 250712/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 250712/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -6,10 +6,11 @@ answer = []
 def recur(num):
     if num == N:
         print(*answer)
+        return
     
     for i in range(1, K+1):
         if len(answer) < 2 or \
-            not (answer[-1] == answer[-2] == i):
+            not (answer[-2] == answer[-1] == i):
             answer.append(i)
             recur(num+1)
             answer.pop()
